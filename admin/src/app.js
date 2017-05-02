4b56fd5513b6cea6b0f446e8a9cd9e2a50f472da
xyxiao <463618622@qq.com>
2017-05-02 16:47:53 +0800

    添加为二级目录

--- admin/src/app.js
+++ admin/src/app.js
@@ -64,7 +64,7 @@ export default class App extends Component {
   }
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/admin/">
         <div className="page">
           <Route exact path="/" render={() => <Redirect to="/dashboard" />}/>
           <Route path="/login" component={Login} />
