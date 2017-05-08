bf067ddaf0edf8b7c14f54177879559e00751ade
xyxiao <463618622@qq.com>
2017-05-08 10:43:24 +0800

    vue 懒加载

--- view/src/router/index.js
+++ view/src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Dashboard from '@/view/Dashboard'
-import Detail from '@/view/ArticleDetail'
+// import Dashboard from '@/view/Dashboard'
+// import Detail from '@/view/ArticleDetail'
 
 Vue.use(Router)
+
+const Dashboard = r => require.ensure([], () => r(require('@/view/Dashboard')), 'group-dashboard')
+const Detail = r => require.ensure([], () => r(require('@/view/ArticleDetail')), 'group-detail')
 
 export default new Router({
   mode: 'hash',
